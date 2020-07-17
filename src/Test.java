4f2a285a901d7769320fec5af5d7545d2a3c0461
ferdi <ferdi@192.168.1.3>
2020-07-17 10:39:14 +0900

    修正

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+package java_book_example;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+
+		int[] seq=new int[10];
+		for(int i=0;i<seq.length;i++) {
+			seq[i]=new java.util.Random().nextInt(4);
+			System.out.println("test");
+		}
+
+
+	}
+
+}
